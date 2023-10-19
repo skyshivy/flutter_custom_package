c32915a8b60fbe62dd7d0d35bd5eba057cf1e5a6
Shiv <sky.shivy@gmail.com>
2023-10-19 15:38:26 +0530

    new

--- lib/flutter_custom_package.dart
+++ lib/flutter_custom_package.dart
@@ -3,5 +3,8 @@ library flutter_custom_package;
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  int addOne(int value) {
+    print("object");
+    return value + 1;
+  }
 }
