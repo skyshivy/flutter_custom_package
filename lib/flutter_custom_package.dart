515f35f05a78f75db7c65a166381eae64b7cfbc5
Shiv <sky.shivy@gmail.com>
2023-10-19 15:45:36 +0530

    new

--- lib/flutter_custom_package.dart
+++ lib/flutter_custom_package.dart
@@ -1,10 +1,32 @@
 library flutter_custom_package;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) {
     print("object");
     return value + 1;
+  }
+}
+
+class SKYButton extends StatelessWidget {
+  final String title;
+  final String message;
+  final Function() onTap;
+  const SKYButton(
+      {super.key,
+      required this.title,
+      required this.message,
+      required this.onTap});
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {
+          onTap();
+          print(message);
+        },
+        child: Text(title));
   }
 }
